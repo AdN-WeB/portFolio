48be950f751c99328a5560b3c2faf37632d84c3b
adrien <adrien.mervelay84@gmail.com>
2021-12-13 15:04:19 +0100

    ajout mentions légales, ancre hyperlink for mention légale, css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { LegalMentionComponent } from './legal-mention/legal-mention.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'home/#pop-up', component: PopUpComponent },
+  { path: 'legal', component: LegalMentionComponent },
 ];
 
 @NgModule({
